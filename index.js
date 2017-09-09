ee6dd63105d10764e30c7a37c5b0ed0cfe524544
Enrico Mattiazzi <enrico.mattiazzi@gmail.com>
2017-09-09 10:53:01 +0100

    update libraries

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-
+const cors = require('cors');
 const mongoose = require('./config/db.config');
 const env = require('./config/env.config.js')
 const app = express();
@@ -11,6 +11,9 @@ const MONGODB_URI = process.env.MONGODB_URI
 const PORT = process.env.PORT
 
 // Middlewares
+app.use(cors({
+  exposedHeaders: ['x-auth']
+}));
 app.use(bodyParser.json());
 if (process.env.NODE_ENV !== 'test') {
   app.use(morgan('dev'));
